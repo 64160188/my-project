6204d4f89992af7a29746687dde9f591fb2884fc
64160188 <64160188@go.buu.ac.th>
2024-06-10 02:01:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+var express = require('express');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var session = require('express-session');
+
+var app = express();
+
+// บอกให้ express ใช้ folder public
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // Ensure this line points to your views directory
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.listen(3000, function() {
+    console.log('Server is running on port 3000');
+});
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/equipment', (req, res) => {
+    res.render('pages/equipment');
+});
